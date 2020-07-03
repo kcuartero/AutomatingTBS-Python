ea3183b91d227d4ebf0f60960a74c7ffb6a95bcf
kcuartero <kevin.cuartero@gmail.com>
2020-07-02 20:55:45 -0400

    Ch 6 - Strings (incomplete)

---
+++ tablePrinter.py
@@ -0,0 +1,7 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana']
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+ def printTable(items)                                            
+    colWidths = [0] * len(tableData)
+    
